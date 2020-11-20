0fed222cc4ed1906d9062e1ed828b7802d474072
rohanxx <rs502693@gmail.com>
2020-11-20 04:14:11 -0800

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'QrCode';
+  term = 'test';
+
+  onInputChange(value: string) {
+    this.term = value;
+  }
 }
